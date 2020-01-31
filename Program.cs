a7fb9a2252fe4c63b88f5474ec8d17b6eda291ed
JulietaLezcano <60350457+JulietaLezcano@users.noreply.github.com>
2020-01-31 09:36:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Linq;
+
+using System.Collections.Generic;
+
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dado un valor numérico entero m, determinar e imprimir un listado con los m primeros múltiplos de 3 que no sean múltiplos de 5
+            List <int> multiplos = new List <int>();
+            Console.WriteLine("Indique el numero de multiplos de 3 que desea obtener");
+            int m= Convert.ToInt32(Console.ReadLine());
+            
+            for (int i=0; i<multiplos.Count(); i++)
+            {
+                
+                   do{
+                        multiplos.Add(i);
+                     } while (i%3==0 & i%5!=0);
+                     {
+                        foreach(var num in multiplos)
+            
+                        Console.WriteLine("{}",num);
+            
+                     }
+             
+                         
+
+
+                   
+                
+                   
+
+                    
+
+                
+            }
+
+
+        }
+    }
+}
